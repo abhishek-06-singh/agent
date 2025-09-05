26daf0356603104ff9f8b080cf2c5485d8e93078
abhi789837 <abhishek.chauhan@oyelabs.com>
2025-09-05 11:22:48 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   eslint: {
+  eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['images.unsplash.com', 'plus.unsplash.com','randomuser.me'],
+    domains: ['images.unsplash.com', 'plus.unsplash.com', 'randomuser.me'],
     unoptimized: true,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-};
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      resourceQuery: /url/, // *.svg?url
+      type: 'asset/resource',
+    })
+    return config
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
